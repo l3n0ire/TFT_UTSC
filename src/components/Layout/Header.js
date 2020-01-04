bfce32317d0d1119ad4e1f41f2856e6b0faa1c76
Colin Lin <colincool100@gmail.com>
2020-01-04 16:42:51 -0500

    header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,26 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import './Header.css'
 
 function Header() {
     return (
-        <header style={headerStyle}>
+        <header>
             <h1>Teamfight UTSC</h1>
-            <Link style = {linkStyle} to='/'>Home</Link>
-            |
-            <Link style = {linkStyle} to='/about'>About</Link>
+            <div className="links">
+            <Link className="link-style" to='/'>Home</Link>
+            
+            <Link className="link-style" to='/about'>About</Link>
+            </div>
 
         </header>
         
     )
 } 
-const linkStyle={
-    color:'#fff',
-    textDecoration:"none"
-}
-const headerStyle={
-    background:"black",
-    color:'#fff',
-    textAlign:'center',
-    padding:'10px'
-}
+
 export default Header
