099c2c84f47fbb9dfcee70e91b297ad5b2bc394a
Colin Lin <colincool100@gmail.com>
2020-01-04 17:07:38 -0500

    csec rip

--- src/components/pages/Index.js
+++ src/components/pages/Index.js
@@ -10,6 +10,15 @@ function Index() {
         </div>
         <div className="container">
             <h1>Who are we?</h1>
+            <p>
+                We’re a group with a strong passion for the game Teamfight Tactics. Teamfight UTSC  holds tournaments 
+                and tutorials for both newcomers and experienced players. Our goal is to provide training sessions, 
+                guides, and tournaments to help players develop their skills and improve their gameplay. Teamfight 
+                UTSC’s mission is to introduce new players to competitive or casual Teamfight Tactics.
+
+            </p>
+            <h1>What we do.</h1>
+
         </div>
         
 
