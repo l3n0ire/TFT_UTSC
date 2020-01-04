d976806731d598c11e0273767a543550e18d2db0
Colin Lin <colincool100@gmail.com>
2020-01-04 14:31:20 -0500

    starter

---
+++ build/precache-manifest.c15f92b12fc065ef82564313db962713.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd3a9e4f8ec173f9df6d03a597fc482",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f34368ba1dbd74b69222",
+    "url": "/static/css/main.eed491e1.chunk.css"
+  },
+  {
+    "revision": "16615339e87350065427",
+    "url": "/static/js/2.e57ba552.chunk.js"
+  },
+  {
+    "revision": "f34368ba1dbd74b69222",
+    "url": "/static/js/main.7a35dcc2.chunk.js"
+  },
+  {
+    "revision": "c05466ba8db6ba4370de",
+    "url": "/static/js/runtime-main.ab8ed807.js"
+  }
+]);
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Fragment} from 'react';
+
+function About() {
+    return (
+        <Fragment>
+            <h1> About </h1>
+            <p>There is some test here</p>
+        </Fragment>
+    )
+}
+export default About;
