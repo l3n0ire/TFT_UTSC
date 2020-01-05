002f29dcfa543a62d62e9fd57268c6ed3e2a80c0
Chewah <chewah@users.noreply.github.com>
2020-01-04 21:49:37 -0500

    Animation for Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Transition } from 'react-transition-group';
 
 import Header from './components/Layout/Header';
 import About from './components/pages/About';
