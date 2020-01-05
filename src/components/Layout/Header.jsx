8eb32ea59c6ce89e2fc4fbcbdededabd6a26fab4
Chewah <chewah@users.noreply.github.com>
2020-01-04 22:48:04 -0500

    Font stuff

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -5,10 +5,10 @@ import './Header.css'
 function Header(props) {
     return (
         <header style={{'background': props.colour}}>
-            <h1>Teamfight UTSC</h1>
-            <Link className="link-style" to='/'>Home</Link>
+            <h1>TEAMFIGHT UTSC</h1>
+            <Link className="link-style" to='/'>HOME</Link>
             
-            <Link className="link-style" to='/about'>About</Link>
+            <Link className="link-style" to='/about'>ABOUT</Link>
 
         </header>
         
